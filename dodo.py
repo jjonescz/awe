168822e79fd17270fc3d302cd6b86f072a30c1a3
Jan Joneš <jjones@outlook.cz>
2021-10-06 09:22:33 +0000

    Add SWDE download progress and extraction

--- dodo.py
+++ dodo.py
@@ -3,12 +3,16 @@
 import urllib.request
 import pathlib
 import os
+import shutil
+import sys
 
 DOIT_CONFIG = {
     'verbosity': 2
 }
 SWDE_URL = 'https://codeplexarchive.blob.core.windows.net/archive/projects/swde/swde.zip'
-SWDE_ZIP = 'data/swde.zip'
+DATA_DIR = 'data'
+SWDE_ZIP = f'{DATA_DIR}/swde.zip'
+SWDE_DIR = f'{DATA_DIR}/swde'
 
 def task_install():
     """pip install"""
@@ -18,15 +22,34 @@ def task_install():
         'file_dep': ['requirements.txt']
     }
 
-def task_download():
+def task_download_swde():
     """download SWDE dataset"""
+
+    def report(blocknr, blocksize, size):
+        # Inspired by https://stackoverflow.com/a/21363808.
+        current = blocknr * blocksize
+        sys.stdout.write("\r{0:.2f}%".format(100.0 * current / size))
 
     def download():
         pathlib.Path(os.path.dirname(SWDE_ZIP)).mkdir(parents=True, exist_ok=True)
-        urllib.request.urlretrieve(SWDE_URL, SWDE_ZIP)
+        print(f'Downloading {SWDE_URL} to {SWDE_ZIP}')
+        urllib.request.urlretrieve(SWDE_URL, SWDE_ZIP, report)
+        print()
 
     return {
         'actions': [download],
         'targets': [SWDE_ZIP],
         'uptodate': [True]
+    }
+
+def task_extract_swde():
+    """extract SWDE zip"""
+
+    def extract():
+        shutil.unpack_archive(SWDE_ZIP, SWDE_DIR)
+
+    return {
+        'actions': [extract],
+        'file_dep': [SWDE_ZIP],
+        'targets': [SWDE_DIR]
     }
