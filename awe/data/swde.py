a854e643e4fc9a12e30a29f022f1687ed85f4a90
Jan Joneš <jjones@outlook.cz>
2021-10-11 12:35:46 +0000

    Add groundtruth parsing

--- awe/data/swde.py
+++ awe/data/swde.py
@@ -18,13 +18,19 @@ PAGE_REGEX = r'^(\d{4})\.htm$'
 BASE_TAG_REGEX = r'^<base href="([^\n]*)"/>\w*\n(.*)'
 GROUNDTRUTH_REGEX = r'^(\w+)-(\w+)-(\w+)\.txt$'
 
+def add_field(**kwargs):
+    return field(hash=False, compare=False, **kwargs)
+
 def ignore_field(**kwargs):
-    return field(init=False, repr=False, hash=False, compare=False, **kwargs)
+    return add_field(init=False, repr=False, **kwargs)
+
+def cache_field(**kwargs):
+    return ignore_field(default=None, **kwargs)
 
 @dataclass
 class Vertical:
     name: str
-    _websites: list['Website'] = ignore_field(default=None)
+    _websites: list['Website'] = cache_field()
 
     @property
     def websites(self):
@@ -43,15 +49,73 @@ class Vertical:
         return f'{DATA_DIR}/{self.name}'
 
     @property
+    def groundtruth_dir(self):
+        return f'{DATA_DIR}/{GROUND_TRUTH}/{self.name}'
+
+    @property
     def groundtruth_path_prefix(self):
-        return f'{DATA_DIR}/{GROUND_TRUTH}/{self.name}/{self.name}'
+        return f'{self.groundtruth_dir}/{self.name}'
+
+@dataclass
+class GroundTruthField:
+    site: 'Website'
+    name: str
+    _entries: list['GroundTruthEntry'] = cache_field()
+
+    def __init__(self, site: 'Website', file_name: str):
+        self.site = site
+        match = re.search(GROUNDTRUTH_REGEX, file_name)
+        assert match.group(1) == site.vertical.name
+        assert match.group(2) == site.name
+        self.name = match.group(3)
+
+    @property
+    def file_name(self):
+        return f'{self.site.vertical.name}-{self.site.name}-{self.name}.txt'
+
+    @property
+    def file_path(self):
+        return f'{self.site.vertical.groundtruth_dir}/{self.file_name}'
+        
+    @property
+    def lines(self):
+        with open(self.file_path, mode='r', encoding='utf-8-sig') as file:
+            return [line.rstrip('\r\n') for line in file]
+
+    @property
+    def entries(self):
+        if self._entries is None:
+            self._entries = list(self._iterate_entries())
+        return self._entries
+
+    def _iterate_entries(self):
+        lines = self.lines
+
+        # Read first line.
+        vertical, site, name = lines[0].split('\t')
+        assert vertical == self.site.vertical.name
+        assert site == self.site.name
+        assert name == self.name
+
+        # Read second line.
+        count, *_ = lines[1].split('\t')
+        assert int(count) == self.site.page_count
+
+        # Read rest of the file.
+        for index, (line, page) in enumerate(zip(lines[2:], self.site.pages)):
+            expected_index, expected_nonnull_count, *values = line.split('\t')
+            assert int(expected_index) == index
+            parsed_values = [] if values == ['<NULL>'] else values
+            assert int(expected_nonnull_count) == len(parsed_values)
+            yield GroundTruthEntry(self, page, parsed_values)
 
 @dataclass
 class Website:
     vertical: Vertical
     name: str
     page_count: int
-    _pages: list['Page'] = ignore_field(default=None)
+    _pages: list['Page'] = cache_field()
+    _groundtruth: list[GroundTruthField] = cache_field()
 
     def __init__(self, vertical: Vertical, dir_name: str):
         match = re.search(WEBSITE_REGEX, dir_name)
@@ -86,20 +150,24 @@ class Website:
         return f'{self.vertical.groundtruth_path_prefix}-{self.name}'
 
     @property
-    def fields(self):
+    def groundtruth(self):
+        if self._groundtruth is None:
+            self._groundtruth = list(self._iterate_groundtruth())
+        return self._groundtruth
+
+    def _iterate_groundtruth(self):
         for file in glob.glob(f'{self.groundtruth_path_prefix}-*.txt'):
             file_name = os.path.basename(file)
-            match = re.search(GROUNDTRUTH_REGEX, file_name)
-            assert match.group(1) == self.vertical.name
-            assert match.group(2) == self.name
-            yield match.group(3)
+            groundtruth_field = GroundTruthField(self, file_name)
+            assert groundtruth_field.file_path == file
+            yield groundtruth_field
 
 @dataclass
 class Page:
     site: Website
     index: int
-    _url: str = ignore_field(default=None)
-    _html: parsel.Selector = ignore_field(default=None)
+    _url: str = cache_field()
+    _html: parsel.Selector = cache_field()
 
     def __init__(self, site: Website, file_name: str):
         match = re.search(PAGE_REGEX, file_name)
@@ -137,6 +205,12 @@ class Page:
         if self._html is None:
             self.parse()
         return self._html
+
+@dataclass
+class GroundTruthEntry:
+    field: GroundTruthField
+    page: Page
+    values: list[str] = add_field()
 
 VERTICALS = [
     Vertical('auto'),
