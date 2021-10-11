fa15142a61295dcacb7fa1bd164c9099e5c2fc7d
Jan Joneš <jjones@outlook.cz>
2021-10-11 07:28:35 +0000

    Add SWDE website dir name parsing

--- awe/data/swde.py
+++ awe/data/swde.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+import os
+import re
+
+SWDE_URL = 'https://codeplexarchive.blob.core.windows.net/archive/projects/swde/swde.zip'
+DATA_DIR = 'data'
+SWDE_ZIP = f'{DATA_DIR}/swde.zip'
+SWDE_DIR = f'{DATA_DIR}/swde'
+SWDE_DATA_DIR = f'{SWDE_DIR}/data'
+
 VERTICALS = [
     "auto",
     "book",
@@ -8,6 +18,22 @@ VERTICALS = [
     "restaurant",
     "university"
 ]
+
+WEBSITE_REGEX = r"^(\w+)-(\w+)\((\d+)\)$"
+
+@dataclass
+class Website:
+    vertical: str
+    name: str
+    page_count: int
+
+def parse_website(dir_name):
+    match = re.search(WEBSITE_REGEX, dir_name)
+    return Website(match.group(1), match.group(2), int(match.group(3)))
+
+def get_websites(vertical):
+    for subdir in os.listdir(f'{SWDE_DATA_DIR}/{vertical}'):
+        yield parse_website(subdir)
 
 WEBSITES = {
     "auto": [
--- dodo.py
+++ dodo.py
@@ -7,10 +7,11 @@ import awe.data.swde
 DOIT_CONFIG = {
     'verbosity': 2
 }
-SWDE_URL = 'https://codeplexarchive.blob.core.windows.net/archive/projects/swde/swde.zip'
-DATA_DIR = 'data'
-SWDE_ZIP = f'{DATA_DIR}/swde.zip'
-SWDE_DIR = f'{DATA_DIR}/swde'
+SWDE_URL = awe.data.swde.SWDE_URL
+DATA_DIR = awe.data.swde.DATA_DIR
+SWDE_ZIP = awe.data.swde.SWDE_ZIP
+SWDE_DIR = awe.data.swde.SWDE_DIR
+SWDE_DATA_DIR = awe.data.swde.SWDE_DATA_DIR
 
 def shell(command: str):
     print(f'$ {command}')
@@ -61,7 +62,7 @@ def task_extract_swde_verticals():
 
     input_dir = f'{SWDE_DIR}/src'
     input_zips = [f'{input_dir}/{v}.7z' for v in awe.data.swde.VERTICALS]
-    output_dir = f'{SWDE_DIR}/data'
+    output_dir = SWDE_DATA_DIR
     output_dirs = [f'{output_dir}/{v}' for v in awe.data.swde.VERTICALS]
     def extract_src():
         for archive in input_zips:
