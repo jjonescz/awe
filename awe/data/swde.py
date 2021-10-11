e232a511d7fa02090d48cce14261e9b0ba939455
Jan Joneš <jjones@outlook.cz>
2021-10-11 08:49:01 +0000

    Use class `Vertical` inside `Website`

--- awe/data/swde.py
+++ awe/data/swde.py
@@ -2,7 +2,7 @@ import os
 import re
 from dataclasses import dataclass, field
 
-import constants
+from . import constants
 
 URL = 'https://codeplexarchive.blob.core.windows.net/archive/projects/swde/swde.zip'
 ZIP = f'{constants.DATA_DIR}/swde.zip'
@@ -22,28 +22,29 @@ class Vertical:
     @property
     def websites(self):
         if self._websites is None:
-            self._websites = [w for w in get_websites(self.name)]
+            self._websites = [w for w in get_websites(self)]
         return self._websites
 
 @dataclass
 class Website:
-    vertical: str
+    vertical: Vertical
     name: str
     page_count: int
 
-    def __init__(self, dir_name: str):
+    def __init__(self, vertical: Vertical, dir_name: str):
         match = re.search(WEBSITE_REGEX, dir_name)
-        self.vertical = match.group(1)
+        assert vertical.name == match.group(1)
+        self.vertical = vertical
         self.name = match.group(2)
         self.page_count = int(match.group(3))
 
     @property
     def dir_name(self):
-        return f'{self.vertical}-{self.name}({self.page_count})'
+        return f'{self.vertical.name}-{self.name}({self.page_count})'
 
-def get_websites(vertical: str):
-    for subdir in os.listdir(f'{DATA_DIR}/{vertical}'):
-        website = Website(subdir)
+def get_websites(vertical: Vertical):
+    for subdir in os.listdir(f'{DATA_DIR}/{vertical.name}'):
+        website = Website(vertical, subdir)
         assert website.dir_name == subdir
         yield website
 
