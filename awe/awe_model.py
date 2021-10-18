14485f706a6d007721a1f78acaf8ee4f67080184
Jan Joneš <jjones@outlook.cz>
2021-10-18 09:29:33 +0000

    Use weighted loss

--- awe/awe_model.py
+++ awe/awe_model.py
@@ -6,7 +6,7 @@ from torch import nn
 
 
 class AweModel(pl.LightningModule):
-    def __init__(self, label_count):
+    def __init__(self, label_count, label_weights):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Linear(2, 32),
@@ -15,6 +15,7 @@ class AweModel(pl.LightningModule):
             nn.ReLU(),
             nn.Linear(32, label_count)
         )
+        self.label_weights = torch.FloatTensor(label_weights)
 
     def forward(self, x):
         return self.layers(x)
@@ -23,7 +24,7 @@ class AweModel(pl.LightningModule):
         x, y = batch
         x = x.view(x.size(0), -1)
         z = self.forward(x)
-        loss = F.cross_entropy(z, torch.max(y, 1)[1])
+        loss = self.criterion(z, y)
         self.log("train_loss", loss)
         return loss
 
@@ -31,12 +32,15 @@ class AweModel(pl.LightningModule):
         x, y = batch
         x = x.view(x.size(0), -1)
         z = self.forward(x)
-        loss =  F.cross_entropy(z, torch.max(y, 1)[1])
+        loss =  self.criterion(z, y)
         preds = torch.argmax(z, dim=1)
         acc = plf.accuracy(preds, torch.max(y, 1)[1])
         self.log("val_loss", loss, prog_bar=True)
         self.log("val_acc", acc, prog_bar=True)
         return loss
+
+    def criterion(self, z, y):
+        return F.cross_entropy(z, torch.max(y, 1)[1], weight=self.label_weights)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
