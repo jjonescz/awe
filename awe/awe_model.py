f59c6010a88e8c3b1c74746d2dfd700f067163fa
Jan Joneš <jjones@outlook.cz>
2021-10-19 06:42:54 +0000

    Report also page-wide metrics

--- awe/awe_model.py
+++ awe/awe_model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
@@ -34,13 +35,26 @@ class AweModel(pl.LightningModule):
         z = self.forward(batch.x)
         loss =  self.criterion(z, y)
         preds = torch.argmax(z, dim=1)
+
+        def metric(f, mask):
+            return f(preds[mask], y[mask])
+
+        def page_metrics(f):
+            """Computes metrics for each page (graph) separately."""
+            return [
+                metric(f, batch.batch == page)
+                for page in range(batch.num_graphs)
+            ]
+
+        accs = page_metrics(metrics.accuracy)
+        f1s = page_metrics(metrics.f1)
         acc = metrics.accuracy(preds, y)
-        nt_acc = 0 if sum(y != 0) == 0 else metrics.accuracy(preds[y != 0], y[y != 0])
         f1 = metrics.f1(preds, y, average="weighted", num_classes=self.label_count, ignore_index=0)
         self.log("val_loss", loss, prog_bar=True)
         self.log("val_acc", acc, prog_bar=True)
-        self.log("val_nt_acc", nt_acc, prog_bar=True)
         self.log("val_f1", f1, prog_bar=True)
+        self.log("val_page_acc", np.mean(accs), prog_bar=True)
+        self.log("val_page_f1", np.mean(f1s), prog_bar=True)
         return loss
 
     def criterion(self, z, y):
