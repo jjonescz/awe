c5d77ef6222eb8a850ae6dc5bbd274793e43523e
Jan Joneš <jjones@outlook.cz>
2021-10-26 13:29:58 +0000

    Intercept requests

--- js/index.ts
+++ js/index.ts
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer-core';
+import path from 'path';
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -8,5 +9,21 @@ import puppeteer from 'puppeteer-core';
     ],
     executablePath: 'google-chrome-stable'
   });
+
+  // Intercept requests.
+  const page = await browser.newPage();
+  page.setRequestInterception(true);
+  page.on('request', request => {
+    console.log('request: ', request.url());
+    if (request.url().startsWith('file://'))
+      request.continue();
+    else
+      request.abort();
+  });
+
+  // Open a page (hard-coded path for now).
+  const fullPath = path.resolve('../data/swde/data/auto/auto-aol(2000)/0000.htm');
+  await page.goto(`file://${fullPath}`);
+
   await browser.close();
 })();
