5f91f477d07cbe07c8ca08ac0fc9e3c8efc832ed
Jan Joneš <jjones@outlook.cz>
2021-10-27 10:03:34 +0000

    Wait more before saving screenshot

--- js/index.ts
+++ js/index.ts
@@ -7,12 +7,18 @@ import { replaceExtension } from './lib/utils';
   // Open browser.
   const scraper = await Scraper.create();
 
+  // TODO: Add offline mode.
+
   try {
     // Open a page (hard-coded path for now).
     const fullPath = path.join(SWDE_FOLDER, 'auto/auto-aol(2000)/0000.htm');
     const page = await SwdePage.parse(fullPath);
     console.log('goto:', fullPath);
     await scraper.go(page);
+
+    // Wait for few more seconds.
+    console.log('waiting 5 seconds');
+    await new Promise((resolve) => setTimeout(resolve, 5_000));
 
     // Take screenshot.
     const screenshotPath = replaceExtension(fullPath, '.png');
