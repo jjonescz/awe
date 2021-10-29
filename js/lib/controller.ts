6d9a6a160d8520c987f675f14b7d34d2e9c96232
Jan Joneš <jjones@outlook.cz>
2021-10-29 07:58:17 +0000

    Add enum `ScrapeVersion`

--- js/lib/controller.ts
+++ js/lib/controller.ts
@@ -1,15 +1,36 @@
 import { Scraper, SwdeHandling, SwdePage } from './scraper';
 import { replaceExtension } from './utils';
 
+export const enum ScrapeVersion {
+  /** Scrapes version that preserves HTML from the SWDE dataset. */
+  Exact,
+  /**
+   * Scrapes latest available version from the WaybackMachine. This doesn't
+   * consider HTML from the SWDE dataset and therefore gold labels might be
+   * inconsistent.
+   */
+  Latest,
+}
+
+export function scrapeVersionToSwdeHandling(version: ScrapeVersion) {
+  switch (version) {
+    case ScrapeVersion.Exact:
+      return SwdeHandling.Offline;
+    case ScrapeVersion.Latest:
+      return SwdeHandling.Wayback;
+  }
+}
+
 /** {@link Scraper} controller to scrape one {@link SwdePage}. */
 export class Controller {
   public constructor(public readonly scraper: Scraper) {}
 
   /** Scrapes {@link SwdePage} determined by {@link fullPath}. */
-  public async scrape(fullPath: string, { latest = false } = {}) {
-    this.scraper.swdeHandling = latest
-      ? SwdeHandling.Wayback
-      : SwdeHandling.Offline;
+  public async scrape(
+    fullPath: string,
+    { version = ScrapeVersion.Exact } = {}
+  ) {
+    this.scraper.swdeHandling = scrapeVersionToSwdeHandling(version);
 
     // Navigate to the page.
     const page = await SwdePage.parse(fullPath);
@@ -22,13 +43,13 @@ export class Controller {
     // Report stats.
     console.log('stats:', this.scraper.stats);
 
-    if (latest && page.timestamp === null) {
+    if (version === ScrapeVersion.Latest && page.timestamp === null) {
       // Couldn't find snapshot of this page in the archive, abort early.
       return;
     }
 
     // Take screenshot.
-    const suffix = latest ? `.${page.timestamp}` : '';
+    const suffix = version === ScrapeVersion.Latest ? `.${page.timestamp}` : '';
     const screenshotPath = replaceExtension(`${fullPath}${suffix}`, '.png');
     console.log('screenshot:', screenshotPath);
     await this.scraper.page.screenshot({
@@ -42,7 +63,7 @@ export class Controller {
 
   /** Scrapes old and new versions of {@link SwdePage} at {@link fullPath}. */
   public async scrapeBoth(fullPath: string) {
-    await this.scrape(fullPath, { latest: false });
-    await this.scrape(fullPath, { latest: true });
+    await this.scrape(fullPath, { version: ScrapeVersion.Exact });
+    await this.scrape(fullPath, { version: ScrapeVersion.Latest });
   }
 }
