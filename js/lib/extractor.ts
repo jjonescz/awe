bb99717052384ff767071eb079b4aaeecca2799f
Jan Joneš <jjones@outlook.cz>
2021-11-01 14:15:03 +0000

    Add `DomData` structure

--- js/lib/extractor.ts
+++ js/lib/extractor.ts
@@ -1,15 +1,32 @@
 import { Page } from 'puppeteer-core';
 import { logger } from './logging';
 
+type DomData = {
+  [key: string]: string | DomData;
+};
+
 /** Can extract visual attributes from a Puppeteer-controlled page. */
 export class Extractor {
+  public readonly data: DomData = {};
+
   constructor(public readonly page: Page) {}
 
   public async extract() {
-    const rootElements = await this.page.$x('/*');
-    for (const element of rootElements) {
-      const tagName = await element.evaluate((e) => e.tagName);
-      logger.info('root', { tagName });
+    const rootElements = await this.page.$x('*');
+    const queue = rootElements.map((e) => ({
+      element: e,
+      parent: this.data,
+    }));
+    while (queue.length !== 0) {
+      const { element, parent } = queue.pop()!;
+      const info = await element.evaluate((e) => {
+        return { tagName: e.tagName.toLowerCase() };
+      });
+      const container: DomData = {};
+      parent[`/${info.tagName}`] = container;
+      const children = await element.$x('*');
+      queue.push(...children.map((e) => ({ element: e, parent: container })));
     }
+    logger.info('data', this.data);
   }
 }
