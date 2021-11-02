77ef90d6f941d1d59b0fbfdb0da164a28aed7537
Jan Joneš <jjones@outlook.cz>
2021-11-02 09:18:35 +0000

    Include more information in file log

--- js/lib/logging.ts
+++ js/lib/logging.ts
@@ -6,9 +6,17 @@ const logFile = path.join(DATA_FOLDER, 'scraping-logs.txt');
 
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
-    new winston.transports.File({ filename: logFile }),
+    new winston.transports.File({
+      filename: logFile,
+      level: 'debug',
+      // Truncate the file before every run.
+      options: { flags: 'w' },
+    }),
     new winston.transports.Console({ format: winston.format.simple() }),
   ],
 });
