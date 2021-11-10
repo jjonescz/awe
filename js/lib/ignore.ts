da4c5f20ee9b0a309b2bd8e676e30e424459bdd3
Jan Joneš <jjones@outlook.cz>
2021-11-10 08:03:06 +0000

    Add more ignore prefixes for AOL website

--- js/lib/ignore.ts
+++ js/lib/ignore.ts
@@ -1,10 +1,17 @@
 /** Patterns of URLs that will be ignored while scraping. */
-export const ignorePatterns = [
-  // Analytics from `auto-aol` website.
-  /&mboxTime=\d+&/,
-  /^http:\/\/a\.vast\.com\/impressions/,
+export const ignorePatterns: RegExp[] = [];
+
+/** Prefixes of URLs that will be ignored while scraping. */
+export const ignoreStartingWith = [
+  // From `auto-aol`.
+  'http://a.vast.com/impressions',
+  'http://tacoda.at.atwola.com/rtx/r.js',
+  'http://aol.tt.omtrdc.net/m2/aol/mbox/',
 ];
 
 export function ignoreUrl(url: string) {
-  return ignorePatterns.some((r) => r.test(url));
+  return (
+    ignoreStartingWith.some((p) => url.startsWith(p)) ||
+    ignorePatterns.some((r) => r.test(url))
+  );
 }
