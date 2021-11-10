b67acf0ce3d5867f7d5eda467e57fb67725e15cc
Jan Joneš <jjones@outlook.cz>
2021-11-10 08:33:43 +0000

    Add more filters for auto websites

--- js/lib/ignore.ts
+++ js/lib/ignore.ts
@@ -7,6 +7,20 @@ export const ignoreStartingWith = [
   'http://a.vast.com/impressions',
   'http://tacoda.at.atwola.com/rtx/r.js',
   'http://aol.tt.omtrdc.net/m2/aol/mbox/',
+  'http://img.vast.com/',
+  // From `auto-autobytel`.
+  'http://www.google-analytics.com/__utm.gif',
+  'http://dp.specificclick.net/',
+  'http://tags.bluekai.com/',
+  'http://www.facebook.com/plugins/like.php',
+  'http://ad.doubleclick.net/',
+  'http://smp.specificmedia.com/smp/',
+  // From `auto-automotive`.
+  'http://secure-us.imrworldwide.com/cgi-bin/j',
+  'http://pbid.pro-market.net/engine',
+  'http://b.scorecardresearch.com/b',
+  'http://aumoautomotivecom.112.2o7.net/b/ss/aumoautomotivecom/1/H.17/',
+  'http://pix04.revsci.net/C07585/b3/0/3/1008211/',
 ];
 
 export function ignoreUrl(url: string) {
