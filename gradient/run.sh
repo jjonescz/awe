8c339d31b85c2661fbca60b054befdaa38696a41
Jan Joneš <jjones@outlook.cz>
2021-11-26 16:44:50 +0100

    Install SSH with huproxy

--- gradient/run.sh
+++ gradient/run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root \
-    --LabApp.trust_xheaders=True --LabApp.disable_check_xsrf=False \
-    --LabApp.allow_remote_access=True --LabApp.allow_origin='*'
+
+# Generate a random alphanumeric string of length 48 (like Jupyter notebook
+# token, e.g., `c8de56fa4deed24899803e93c227592aef6538f93025fe01`). Inspired by
+# https://gist.github.com/earthgecko/3089509.
+if [ -z "$JUPYTER_TOKEN" ]; then
+    JUPYTER_TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 48 | head -n 1)
+fi
+
+# Print mocked Jupyter token, so that we can run this container as if it were a
+# notebook within Gradient.run. Inspired by
+# https://github.com/Paperspace/gradient-coder.
+echo "http://localhost:8888/?token=${JUPYTER_TOKEN}"
+echo "http://localhost:8888/\?token\=${JUPYTER_TOKEN}"
+
+echo "root:${JUPYTER_TOKEN}" | chpasswd && /usr/sbin/sshd -eD &
+/huproxy/bin/huproxy -listen :8888
