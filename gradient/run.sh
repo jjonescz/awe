3723f36ea73d22627bd40290b7bf8b84a35a4a85
Jan Joneš <jjones@outlook.cz>
2021-11-29 14:37:42 +0100

    Configure Git on start

--- gradient/run.sh
+++ gradient/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script should be run after source code is mounted.
+
+# Configure Git.
+./sh/configure.sh
+
 # Generate a random alphanumeric string of length 48 (like Jupyter notebook
 # token, e.g., `c8de56fa4deed24899803e93c227592aef6538f93025fe01`). Inspired by
 # https://gist.github.com/earthgecko/3089509.
