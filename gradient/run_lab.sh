3d41f400c8fb64fac826cf1c0269f530f2188b04
Jan Joneš <jjones@outlook.cz>
2021-11-29 18:56:30 +0100

    Add fallback script for Jupyter Lab

---
+++ gradient/run_lab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+jupyter lab --ip=0.0.0.0 --no-browser --allow-root \
+    --LabApp.trust_xheaders=True --LabApp.disable_check_xsrf=False \
+    --LabApp.allow_remote_access=True --LabApp.allow_origin='*'
