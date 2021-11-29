be68a98576d24bb38c07fc3d5c9460c685dc59ae
Jan Jones <jjones@outlook.cz>
2021-11-29 12:24:14 +0000

    Add VSCode server initialization script

---
+++ gradient/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension streetsidesoftware.code-spell-checker
+
+/opt/conda/bin/conda init bash
