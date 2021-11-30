83e681aa844e7459238306ad9d56f673209b191a
Jan Joneš <jjones@outlook.cz>
2021-11-30 09:07:33 +0000

    Install monitoring extensions

--- gradient/install.sh
+++ gradient/install.sh
@@ -8,3 +8,5 @@ code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension mutantdino.resourcemonitor
+code --install-extension innerlee.nvidia-smi
