8dfe98ee607cbb3a047ab40bd56c8c9353130f79
Jan Joneš <jjones@outlook.cz>
2021-12-07 10:36:31 +0000

    Assert all pages are loaded

--- awe/data/swde.py
+++ awe/data/swde.py
@@ -114,6 +114,8 @@ class GroundTruthField:
             parsed_values = [] if values == ['<NULL>'] else values
             assert int(expected_nonnull_count) == len(parsed_values)
             page = self.site.pages[index]
+            assert page is not None, \
+                f'No page at {index} in {self.site.dir_name}.'
             assert page.index == index
             yield GroundTruthEntry(self, page, parsed_values)
 
