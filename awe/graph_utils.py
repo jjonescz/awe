b47cbedd3fef262da563974f79145de5444a5b3c
Jan Joneš <jjones@outlook.cz>
2021-12-14 11:45:23 +0000

    Link sibling nodes

--- awe/graph_utils.py
+++ awe/graph_utils.py
@@ -25,25 +25,32 @@ def remove_single_nodes(ctx: features.PageContextBase):
 
     return changes
 
-def to_networkx(ctx: features.PageContextBase):
-    graph = nx.DiGraph()
+class PageGraph:
+    def __init__(self, ctx: features.PageContextBase):
+        self.ctx = ctx
+        self.graph = nx.DiGraph()
 
-    for node in ctx.nodes:
-        graph.add_node(
-            node.dataset_index,
-            id=node.element.get('id') or '' if not node.is_text else '',
-            tag=node.tag_name,
-            text=node.text if node.is_text else '',
-            leaf=node.is_text,
-            summary=node.summary,
-            label=node.labels,
-            display_size=5 * (len(node.labels) + 1),
-            coords=(node.box.x, node.box.y) if node.box is not None else (0, 0),
-            center=node.box.center_point if node.box is not None else (0, 0)
-        )
+        for node in ctx.nodes:
+            self.graph.add_node(
+                node.dataset_index,
+                id=node.element.get('id') or '' if not node.is_text else '',
+                tag=node.tag_name,
+                text=node.text if node.is_text else '',
+                leaf=node.is_text,
+                summary=node.summary,
+                label=node.labels,
+                display_size=5 * (len(node.labels) + 1),
+                coords=(node.box.x, node.box.y) if node.box is not None else (0, 0),
+                center=node.box.center_point if node.box is not None else (0, 0)
+            )
 
-    for node in ctx.nodes:
-        if node.parent is not None and node.parent.dataset_index is not None:
-            graph.add_edge(node.dataset_index, node.parent.dataset_index)
+    def link_parents(self):
+        for node in self.ctx.nodes:
+            if node.parent is not None and node.parent.dataset_index is not None:
+                self.graph.add_edge(node.dataset_index, node.parent.dataset_index)
 
-    return graph
+    def link_siblings(self):
+        for node in self.ctx.nodes:
+            siblings = [n for n in node.children if n.dataset_index is not None]
+            for prev, next in zip(siblings, siblings[1:]):
+                self.graph.add_edge(prev.dataset_index, next.dataset_index)
