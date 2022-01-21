81bc446c5c797b56f6bb4453329ae63a6d31e422
Jan Joneš <jan.jones@apify.com>
2022-01-21 09:17:32 +0100

    Print summary on separate lines

--- summarize.py
+++ summarize.py
@@ -3,4 +3,6 @@ from awe import utils
 
 sds = swde.Dataset(suffix='-exact')
 pages = [p for v in sds.verticals for w in v.websites for p in w.pages]
-print(utils.summarize_pages(pages))
+summary = utils.summarize_pages(pages)
+for key, value in summary.items():
+    print(f'{key}: {value}')
