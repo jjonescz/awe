ff8b5c2ba2b70ff8cbf2797d1f673a78062e3e16
Jan Joneš <jjones@outlook.cz>
2022-01-24 09:52:58 +0100

    Configure remote Jupyter only on remote host

--- gradient/install.sh
+++ gradient/install.sh
@@ -2,6 +2,14 @@
 
 # This script should be run once after VSCode is connected.
 
+# Set remote-machine-specific settings.
+cat << EOF > ~/.vscode-server/data/Machine/settings.json
+{
+    "jupyter.jupyterServerType": "remote"
+}
+EOF
+
+# Install extensions.
 code --install-extension editorconfig.editorconfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension ms-python.python
