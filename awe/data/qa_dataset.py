6c9a67b0355c89ef9410593e5177e81aed322cef
Jan Joneš <jjones@outlook.cz>
2022-01-25 16:57:44 +0000

    Decode questions

--- awe/data/qa_dataset.py
+++ awe/data/qa_dataset.py
@@ -7,6 +7,7 @@ import re
 import warnings
 from typing import Iterable, Optional, Union
 
+import numpy as np
 import pandas as pd
 import selectolax.parser
 import torch
@@ -238,18 +239,30 @@ class QaTorchDataset(torch.utils.data.Dataset):
         predictions: list[qa_model.QaModelPrediction]
     ):
         for pred in predictions:
+            input_ids: torch.LongTensor = pred.batch['input_ids'][0, :]
+
+            special_mask = np.array(self.tokenizer.get_special_tokens_mask(
+                input_ids,
+                already_has_special_tokens=True
+            ))
+            special_indices, = np.where(special_mask == 1)
+            question = self.tokenizer.decode(
+                input_ids[special_indices[0] + 1:special_indices[1]]
+            )
+
             gold_start = pred.batch['start_positions'][0]
             gold_end = pred.batch['end_positions'][0]
-            pred_start = torch.argmax(pred.outputs.start_logits)
-            pred_end = torch.argmax(pred.outputs.end_logits)
-            input_ids: torch.LongTensor = pred.batch['input_ids'][0, :]
             gold_answer = self.tokenizer.decode(
                 input_ids[gold_start:gold_end + 1]
             )
+
+            pred_start = torch.argmax(pred.outputs.start_logits)
+            pred_end = torch.argmax(pred.outputs.end_logits)
             pred_answer = self.tokenizer.decode(
                 input_ids[pred_start:pred_end + 1]
             )
-            yield gold_answer, pred_answer
+
+            yield question, gold_answer, pred_answer
 
 def prepare_entries(pages: list[awe_graph.HtmlPage], *,
     skip_existing: bool = True
