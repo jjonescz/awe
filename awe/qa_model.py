98ad32d397dc5d26741114ff14c08075990b5033
Jan Joneš <jjones@outlook.cz>
2022-01-25 13:18:36 +0000

    Add shared `QaPipeline`

--- awe/qa_model.py
+++ awe/qa_model.py
@@ -1,29 +1,54 @@
+from typing import Optional
+
 import datasets
 import pytorch_lightning as pl
 import torch
 import transformers
+from transformers.models.big_bird.modeling_big_bird import \
+    BigBirdForQuestionAnsweringModelOutput
 
 
-# pylint: disable=arguments-differ, unused-argument
-class QaModel(pl.LightningModule):
+class QaPipeline:
+    _model: Optional[transformers.BigBirdForQuestionAnswering] = None
+    _tokenizer: Optional[transformers.PreTrainedTokenizerBase] = None
+
     def __init__(self,
         model_id = 'vasudevgupta/bigbird-roberta-natural-questions'
     ):
+        self.model_id = model_id
+
+    def load(self):
+        _ = self.model
+        _ = self.tokenizer
+
+    @property
+    def model(self) -> transformers.BigBirdForQuestionAnswering:
+        if self._model is None:
+            self._model = transformers.AutoModelForQuestionAnswering \
+                .from_pretrained(self.model_id)
+        return self._model
+
+    @property
+    def tokenizer(self) -> transformers.PreTrainedTokenizerBase:
+        if self._tokenizer is None:
+            self._tokenizer = transformers.AutoTokenizer.from_pretrained(
+                self.model_id)
+        return self._tokenizer
+
+# pylint: disable=arguments-differ, unused-argument
+class QaModel(pl.LightningModule):
+    def __init__(self, pipeline: QaPipeline):
         super().__init__()
 
         self.save_hyperparameters()
 
-        self.model_id = model_id
-        self.model = transformers.AutoModelForQuestionAnswering.from_pretrained(model_id)
+        self.model = pipeline.model
         self.metric = datasets.load_metric('accuracy')
 
     def configure_optimizers(self):
         return transformers.AdamW(self.parameters(), lr=1e-5)
 
-    def create_tokenizer(self):
-        return transformers.AutoTokenizer.from_pretrained(self.model_id)
-
-    def forward(self, encodings):
+    def forward(self, encodings) -> BigBirdForQuestionAnsweringModelOutput:
         return self.model(**encodings)
 
     def training_step(self, batch, *_):
