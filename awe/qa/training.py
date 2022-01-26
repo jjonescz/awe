df498b2dbf69f1f808ad04f54345cdea7b3eb17a
Jan Joneš <jjones@outlook.cz>
2022-01-26 14:38:22 +0000

    Train on smaller subset for now

--- awe/qa/training.py
+++ awe/qa/training.py
@@ -5,12 +5,13 @@ import awe.qa.trainer
 def main():
     params = awe.qa.trainer.QaTrainerParams(
         batch_size=1,
+        train_subset=100,
         version_name='qa-bigger-batch'
     )
     trainer = awe.qa.trainer.QaTrainer(params)
     trainer.load_pipeline()
     trainer.load_data()
-    #trainer.prepare_data()
+    trainer.prepare_data()
     trainer.create_model()
     trainer.delete_previous_version()
     trainer.train()
