aa006a10fb0ad7bf5cee6bc0a7aa6d2975ebad7d
Jan Joneš <jjones@outlook.cz>
2022-01-26 13:38:28 +0000

    Update QA training script

---
+++ awe/qa/training.py
@@ -0,0 +1,19 @@
+# Run: `python -m awe.qa.training`
+
+import awe.qa.trainer
+
+def main():
+    params = awe.qa.trainer.QaTrainerParams(
+        batch_size=1,
+        version_name='qa-bigger-batch'
+    )
+    trainer = awe.qa.trainer.QaTrainer(params)
+    trainer.load_pipeline()
+    trainer.load_data()
+    #trainer.prepare_data()
+    trainer.create_model()
+    trainer.delete_previous_version()
+    trainer.train()
+
+if __name__ == '__main__':
+    main()
