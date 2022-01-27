5be637f0486cc0011c51a58f7162588c99df8dad
Jan Joneš <jjones@outlook.cz>
2022-01-27 15:24:16 +0000

    Add decoder that outputs `pandas.DataFrame`

---
+++ awe/qa/decoder.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import torch
+import transformers
+
+from awe import qa_model
+
+
+class Decoder:
+    def __init__(self,
+        tokenizer: transformers.PreTrainedTokenizerBase
+    ):
+        self.tokenizer = tokenizer
+
+    def decode_predictions(self, predictions: list[qa_model.QaModelPrediction]):
+        return pd.DataFrame(self._iterate_decode_predictions(predictions))
+
+    def _iterate_decode_predictions(self,
+        predictions: list[qa_model.QaModelPrediction]
+    ):
+        for pred in predictions:
+            for row in range(pred.batch['input_ids'].shape[0]):
+                input_ids: torch.LongTensor = pred.batch['input_ids'][row]
+
+                special_mask = np.array(self.tokenizer.get_special_tokens_mask(
+                    input_ids,
+                    already_has_special_tokens=True
+                ))
+                special_indices, = np.where(special_mask == 1)
+                question = self.tokenizer.decode(
+                    input_ids[special_indices[0] + 1:special_indices[1]]
+                )
+
+                gold_start = pred.batch['start_positions'][row]
+                gold_end = pred.batch['end_positions'][row]
+                gold_answer = self.tokenizer.decode(
+                    input_ids[gold_start:gold_end + 1]
+                )
+
+                pred_start = torch.argmax(pred.outputs.start_logits[row])
+                pred_end = torch.argmax(pred.outputs.end_logits[row])
+                pred_answer = self.tokenizer.decode(
+                    input_ids[pred_start:pred_end + 1]
+                )
+
+                yield {
+                    'question': question,
+                    'gold_span': (gold_start.item(), gold_end.item()),
+                    'pred_span': (pred_start.item(), pred_end.item()),
+                    'gold_answer': gold_answer,
+                    'pred_answer': pred_answer,
+                }
