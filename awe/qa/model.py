b12588ad928d5359fb3fff66952ab92812c2a1a2
Jan Joneš <jjones@outlook.cz>
2022-02-08 14:24:40 +0000

    Add custom head for BigBird

--- awe/qa/model.py
+++ awe/qa/model.py
@@ -2,8 +2,11 @@ import dataclasses
 
 import torch
 import transformers
+import transformers.activations
 from transformers.models.big_bird.modeling_big_bird import \
     BigBirdForQuestionAnsweringModelOutput
+from transformers.modeling_outputs import \
+    BaseModelOutputWithPoolingAndCrossAttentions
 
 ModelOutput = BigBirdForQuestionAnsweringModelOutput
 
@@ -18,14 +21,47 @@ class Model(torch.nn.Module):
     ):
         super().__init__()
         self.model = model
+        output_size = model.config.hidden_size # 768
+        self.dropout1 = torch.nn.Dropout(0.1)
+        self.dense1 = torch.nn.Linear(output_size, 3072)
+        self.intermediate_act_fn = transformers.activations.gelu_fast
+        self.dense2 = torch.nn.Linear(3072, 768)
+        self.dropout2 = torch.nn.Dropout(0.1)
+        self.layer_norm = torch.nn.LayerNorm(768, eps=1e-12)
+        self.qa_outputs = torch.nn.Linear(768, 2)
+        # Last token is used for truncated answers, hence it's ignored.
+        self.loss_fn = torch.nn.CrossEntropyLoss(ignore_index=-1)
 
     def configure_optimizers(self):
         return transformers.AdamW(self.parameters(), lr=1e-5)
 
     def forward(self, batch: transformers.BatchEncoding) -> ModelOutput:
-        return self.model(
-            input_ids=batch['input_ids'],
-            attention_mask=batch['attention_mask'],
-            start_positions=batch['start_positions'],
-            end_positions=batch['end_positions'],
+        outputs: BaseModelOutputWithPoolingAndCrossAttentions = self.model.bert(
+            input_ids=batch.input_ids,
+            attention_mask=batch.attention_mask,
+        )
+
+        x = outputs.last_hidden_state # [batch, seq_length, hidden_size]
+        x = self.dropout1(x)
+        x = self.dense1(x) # [batch, seq_length, 3072]
+        x = self.intermediate_act_fn(x)
+        x = self.dense2(x) # [batch, seq_length, 768]
+        x = self.dropout2(x)
+        x = self.layer_norm(x)
+        logits = self.qa_outputs(x) # [batch, seq_length, 2]
+
+        # Effectively ignore question tokens.
+        logits = logits - batch.question_mask.int().unsqueeze(2) * 1e6
+
+        start_logits = logits[:, :, 0] # [batch, seq_length]
+        end_logits = logits[:, :, 1] # [batch, seq_length]
+
+        start_loss = self.loss_fn(start_logits, batch.start_positions)
+        end_loss = self.loss_fn(end_logits, batch.end_positions)
+        total_loss = (start_loss + end_loss) / 2
+
+        return ModelOutput(
+            loss=total_loss,
+            start_logits=start_logits,
+            end_logits=end_logits
         )
