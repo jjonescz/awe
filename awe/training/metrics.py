fc8dc0a0261bd58fd647187a78354e87648f9471
Jan Joneš <jjones@outlook.cz>
2022-02-09 09:11:26 +0000

    Extract code for F1 computation

---
+++ awe/training/metrics.py
@@ -0,0 +1,37 @@
+import dataclasses
+
+import torch
+
+
+@dataclasses.dataclass
+class F1Metrics:
+    precision: float
+    recall: float
+    f1: float
+
+    def to_vector(self):
+        return torch.FloatTensor([self.precision, self.recall, self.f1])
+
+    @staticmethod
+    def from_vector(vector: torch.FloatTensor):
+        return F1Metrics(vector[0].item(), vector[1].item(), vector[2].item())
+
+    @staticmethod
+    def compute(*,
+        true_positives: int,
+        false_positives: int,
+        false_negatives: int
+    ):
+        if (true_positives + false_positives) == 0:
+            precision = 0.0
+        else:
+            precision = true_positives / (true_positives + false_positives)
+        if (true_positives + false_negatives) == 0:
+            recall = 0.0
+        else:
+            recall = true_positives / (true_positives + false_negatives)
+        if (precision + recall) == 0:
+            f1 = 0.0
+        else:
+            f1 = 2 * (precision * recall) / (precision + recall)
+        return F1Metrics(precision, recall, f1)
