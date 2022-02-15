d81f0673a21b824d63b1f33007d905ec08071884
Jan Joneš <jjones@outlook.cz>
2022-02-15 09:00:10 +0000

    Fix circular import

--- awe/data/html_utils.py
+++ awe/data/html_utils.py
@@ -1,13 +1,16 @@
-import awe.data.parsing
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import awe.data.parsing
 
 TEXT_TAG = '-text'
 
-def iter_prev(node: awe.data.parsing.Node):
+def iter_prev(node: 'awe.data.parsing.Node'):
     while node.prev is not None:
         yield node.prev
         node = node.prev
 
-def get_xpath(node: awe.data.parsing.Node):
+def get_xpath(node: 'awe.data.parsing.Node'):
     xpath = ''
     while node.parent is not None:
         # How many nodes of the same tag are there?
@@ -31,8 +34,8 @@ def get_xpath(node: awe.data.parsing.Node):
         node = node.parent
     return xpath
 
-def is_text(node: awe.data.parsing.Node):
+def is_text(node: 'awe.data.parsing.Node'):
     return node.tag == TEXT_TAG
 
-def get_xpath_tag(node: awe.data.parsing.Node):
+def get_xpath_tag(node: 'awe.data.parsing.Node'):
     return 'text()' if is_text(node) else node.tag
