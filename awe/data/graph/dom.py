671a1ce9ad6b916550d2f2eef858d9addd29ea6e
Jan Joneš <jjones@outlook.cz>
2022-02-16 10:51:26 +0000

    Add DOM node wrapper

--- awe/data/graph/dom.py
+++ awe/data/graph/dom.py
@@ -1,3 +1,6 @@
+import dataclasses
+from typing import Callable, Optional
+
 import awe.data.parsing
 import awe.data.set.pages
 
@@ -6,3 +9,32 @@ class Dom:
     def __init__(self, page: awe.data.set.pages.Page):
         self.page = page
         self.tree = awe.data.parsing.parse_html(page.get_html_text())
+
+        # Get all nodes.
+        self.root = Node(parsed=self.tree.body, parent=None)
+        self.nodes = list(self.root.traverse())
+
+@dataclasses.dataclass
+class Node:
+    parsed: awe.data.parsing.Node
+    parent: Optional['Node'] = dataclasses.field(repr=False)
+    children: list['Node'] = dataclasses.field(repr=False, default_factory=list)
+
+    def __post_init__(self):
+        self.children = list(self._iterate_children())
+
+    def _iterate_children(self):
+        for parsed_node in self.parsed.iter(include_text=True):
+            yield Node(parsed=parsed_node, parent=self)
+
+    def traverse(self,
+        shallow_predicate: Callable[['Node'], bool] = lambda _: True,
+        deep_predicate: Callable[['Node'], bool] = lambda _: True,
+    ):
+        stack = [self]
+        while len(stack) != 0:
+            node = stack.pop()
+            if shallow_predicate(node):
+                yield node
+            if deep_predicate(node):
+                stack.extend(node.children)
