50bb0814953ecae546349cdecaf9e86fcd082298
Jan Joneš <jjones@outlook.cz>
2022-03-07 13:50:05 +0000

    Add validation of visuals DOM structure

--- awe/data/validation.py
+++ awe/data/validation.py
@@ -5,18 +5,30 @@ from tqdm.auto import tqdm
 import awe.data.set.pages
 
 
-def validate(pages: list[awe.data.set.pages.Page]):
-    """Checks that label key-value pairs are consistent."""
-
+def validate(
+    pages: list[awe.data.set.pages.Page],
+    labels: bool = True,
+    visuals: bool = True,
+):
     for page in tqdm(pages, desc='pages'):
         page: awe.data.set.pages.Page
-        page_labels = page.get_labels()
-        for key in page_labels.label_keys:
-            values = page_labels.get_label_values(key)
-            nodes = page_labels.get_labeled_nodes(key)
-            expected = len(values)
-            actual = len(nodes)
-            if actual < expected:
-                warnings.warn(
-                    f'Found {actual} < {expected} nodes labeled ' + \
-                    f'{repr(key)}={repr(values)} ({page.html_path}).')
+
+        # Check that label key-value pairs are consistent.
+        if labels:
+            page_labels = page.get_labels()
+            for key in page_labels.label_keys:
+                values = page_labels.get_label_values(key)
+                nodes = page_labels.get_labeled_nodes(key)
+                expected = len(values)
+                actual = len(nodes)
+                if actual < expected:
+                    warnings.warn(
+                        f'Found {actual} < {expected} nodes labeled ' + \
+                        f'{repr(key)}={repr(values)} ({page.html_path}).')
+
+        # Check that extracted visual DOM has the same structure as parsed DOM.
+        if visuals:
+            page_dom = page.dom
+            page_dom.init_nodes()
+            page_visuals = page.load_visuals()
+            page_visuals.fill_tree(page_dom)
