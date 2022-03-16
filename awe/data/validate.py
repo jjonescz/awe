a092835643ae0fe0bd92e155ac984630b69ba1ad
Jan Joneš <jjones@outlook.cz>
2022-03-16 09:04:20 +0000

    Support reading page list during validation

--- awe/data/validate.py
+++ awe/data/validate.py
@@ -1,6 +1,8 @@
 # Run: `python -m awe.data.validate`
 
 import argparse
+import os
+import warnings
 
 import awe.data.set.apify
 import awe.data.set.swde
@@ -20,6 +22,10 @@ parser.add_argument('target',
     nargs='*',
     help='websites (Apify) or verticals (SWDE) to validate'
 )
+parser.add_argument('--read-list',
+    dest='read_list',
+    help='File with list of page paths that will be considered'
+)
 args = parser.parse_args()
 
 if args.dataset == 'apify':
@@ -30,6 +36,24 @@ elif args.dataset == 'swde':
         only_verticals=args.target,
         convert=False
     )
+
 pages = ds.get_all_pages(zip_websites=False)
+
+# Keep only pages from previous list of invalid pages.
+if args.read_list is not None:
+    if os.path.exists(args.read_list):
+        with open(args.read_list, mode='r', encoding='utf-8') as f:
+            page_paths = { l.rstrip() for l in f.readlines() }
+        pages = [
+            p for p in pages
+            if p.html_path in page_paths
+        ]
+        print(
+            f'Found {len(pages)} of {len(page_paths)} pages in the list ' +
+            f'{args.read_list!r}.'
+        )
+    else:
+        warnings.warn(f'List file not found ({args.read_list!r}).')
+
 validator = awe.data.validation.Validator(visuals=False)
 validator.validate_pages(pages)
