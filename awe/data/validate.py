5a4da4cd67de6fc8fdaf271c5a2653bc37ec44a5
Jan Joneš <jjones@outlook.cz>
2022-03-16 08:21:37 +0000

    Add CLI arg for dataset to validate script

--- awe/data/validate.py
+++ awe/data/validate.py
@@ -1,9 +1,27 @@
 # Run: `python -m awe.data.validate`
 
-import awe.data.validation
-import awe.data.set.apify
+import argparse
 
-ds = awe.data.set.apify.Dataset(convert=False)
+import awe.data.set.apify
+import awe.data.set.swde
+import awe.data.validation
+
+parser = argparse.ArgumentParser(
+    description='Validates datasets',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument('-d',
+    dest='dataset',
+    choices=['apify', 'swde'],
+    default='apify',
+    help='dataset'
+)
+args = parser.parse_args()
+
+if args.dataset == 'apify':
+    ds = awe.data.set.apify.Dataset(convert=False)
+elif args.dataset == 'swde':
+    ds = awe.data.set.swde.Dataset(suffix='-exact', convert=False)
 pages = ds.get_all_pages(zip_websites=False)
 validator = awe.data.validation.Validator(visuals=False)
 validator.validate_pages(pages)
