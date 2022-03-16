bc13975256f09a795d2a4ca2484ab64cdfe6e75c
Jan Joneš <jjones@outlook.cz>
2022-03-16 12:58:27 +0000

    Add training param to specify vertical

--- awe/training/trainer.py
+++ awe/training/trainer.py
@@ -92,7 +92,7 @@ class Trainer:
         if self.params.dataset == awe.training.params.Dataset.swde:
             self.ds = awe.data.set.swde.Dataset(
                 suffix='-exact',
-                only_verticals=('auto',)
+                only_verticals=(self.params.vertical,),
             )
         elif self.params.dataset == awe.training.params.Dataset.apify:
             self.ds = awe.data.set.apify.Dataset(
