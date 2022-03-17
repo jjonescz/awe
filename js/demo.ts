6be9acd15c383bebf0ba6625c8bc17cd359191ea
Jan Joneš <jjones@outlook.cz>
2022-03-17 14:34:24 +0000

    Initialize scraper before starting demo server

--- js/demo.ts
+++ js/demo.ts
@@ -1,16 +1,43 @@
 import express from 'express';
+import { logger } from './lib/logging';
+import { Scraper } from './lib/scraper';
 
-const app = express();
-const port = process.env.PORT || 3000;
+logger.level = 'verbose';
 
-app.get(['/', '/:name'], (req, res) => {
-  const greeting = '<h1>Hello From Node</h1>';
-  const name = req.params['name'];
-  if (name) {
-    res.send(`${greeting}<p>Welcome, ${name}.</p>`);
-  } else {
-    res.send(greeting);
-  }
-});
+(async () => {
+  // Open browser (Puppeteer).
+  const scraper = await Scraper.create({
+    poolSize: 1,
+    executablePath: 'google-chrome-stable',
+    devtools: false,
+    timeout: 1000,
+    disableJavaScript: false,
+  });
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}/`));
+  // Create server.
+  const app = express();
+  const port = process.env.PORT || 3000;
+  const log = logger.child({ server: port });
+
+  app.get(['/', '/:name'], (req, res) => {
+    const greeting = '<h1>Hello From Node</h1>';
+    const name = req.params['name'];
+    if (name) {
+      res.send(`${greeting}<p>Welcome, ${name}.</p>`);
+    } else {
+      res.send(greeting);
+    }
+  });
+
+  app.get('/run/:url', async (req, res) => {
+    // Parse parameters.
+    const url = req.params['url'];
+    log.debug('run', { url: url });
+
+    // TODO: Run through Puppeteer.
+  });
+
+  app.listen(port, async () => {
+    console.log(`Listening on http://localhost:${port}/`);
+  });
+})();
