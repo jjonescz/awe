5b96c946a7e0b89ff6797896e261a50890b6464c
Jan Joneš <jjones@outlook.cz>
2022-03-22 14:58:38 +0100

    Consolidate import style of GloVe module

--- awe/data/glove.py
+++ awe/data/glove.py
@@ -2,32 +2,32 @@ import os
 
 from tqdm.auto import tqdm
 
-from awe.data import constants
+import awe.data.constants
 
-GLOVE_DIR = f'{constants.DATA_DIR}/glove'
+GLOVE_DIR = f'{awe.data.constants.DATA_DIR}/glove'
 os.environ['GENSIM_DATA_DIR'] = GLOVE_DIR
 
 # pylint: disable=wrong-import-order, wrong-import-position
-from gensim import downloader as api
-from gensim import models
+import gensim.downloader
+import gensim.models
 
-assert api.base_dir == GLOVE_DIR
+assert gensim.downloader.base_dir == GLOVE_DIR
 
 # See https://github.com/RaRe-Technologies/gensim-data.
 VECTOR_DIMENSION = 100
 MODEL_NAME = f'glove-wiki-gigaword-{VECTOR_DIMENSION}'
 
 def download_embeddings():
-    api.load(MODEL_NAME, return_path=True)
+    gensim.downloader.load(MODEL_NAME, return_path=True)
 
 class LazyEmbeddings:
     """Container for lazily loaded model."""
     _model = None
 
     @classmethod
-    def get_or_create(cls) -> models.KeyedVectors:
+    def get_or_create(cls) -> gensim.models.KeyedVectors:
         if cls._model is None:
             with tqdm(total=1, desc='loading word vectors') as progress:
-                cls._model = api.load(MODEL_NAME)
+                cls._model = gensim.downloader.load(MODEL_NAME)
                 progress.update()
         return cls._model
