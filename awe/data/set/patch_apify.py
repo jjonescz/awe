88e04924922476d357faa96eeb88cf564c9c4f48
Jan Jones <jjones@outlook.cz>
2022-03-28 19:17:10 +0000

    Patch `notinoEn` in database

--- awe/data/set/patch_apify.py
+++ awe/data/set/patch_apify.py
@@ -7,14 +7,15 @@ import awe.data.set.apify
 def main():
     # In Notino, `<noscript>` in head is not HTML compliant, so it gets parsed
     # wrong by Lexbor.
-    ds = awe.data.set.apify.Dataset(only_websites=('notinoEn',), convert=False)
+    ds = awe.data.set.apify.Dataset(only_websites=('notinoEn',))
     w = ds.verticals[0].websites[0]
-    for row in tqdm(w.df.iloc, desc='notinoEn'):
-        html: str = row.localizedHtml
+    for page in tqdm(w.pages, desc='notinoEn'):
+        page: awe.data.set.apify.Page
+        html = page.get_html_text()
         html = html.replace('<noscript>&lt;link', '<noscript><link')
         html = html.replace('&gt;</noscript>', '></noscript>')
-        row.localizedHtml = html
-    w.save_json_df()
+        w.db.replace(page.index, page.url, html, w.db.get_visuals(page.index), w.db.get_metadata(page.index))
+    w.db.save()
 
 if __name__ == '__main__':
     main()
