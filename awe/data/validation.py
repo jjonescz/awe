616940a804204dbcda5b5408c322af76b88c15f5
Jan Jones <jjones@outlook.cz>
2022-03-31 14:19:44 +0000

    Validate that target nodes have bounding box

--- awe/data/validation.py
+++ awe/data/validation.py
@@ -13,6 +13,7 @@ import awe.data.set.pages
 class Validator:
     labels: bool = True
     visuals: bool = True
+    labeled_boxes: bool = True
     num_invalid: int = 0
     file: Optional[TextIOWrapper] = None
 
@@ -38,19 +39,19 @@ class Validator:
             if max_invalid is not None and self.num_invalid >= max_invalid:
                 break
 
+    @staticmethod
+    def get_selector_str(page: awe.data.set.pages.Page, key: str):
+        selector = page.labels.get_selector(key)
+        s = '' if selector is None else f'({selector=})'
+        return f'{key!r}{s}'
+
     def validate_page(self, page: awe.data.set.pages.Page):
         # Check that label key-value pairs are consistent.
         if self.labels:
-            page_labels = page.get_labels()
-
-            def get_selector_str(key: str):
-                selector = page_labels.get_selector(key)
-                return '' if selector is None else f'({selector=})'
-
             total = 0
-            for key in page_labels.label_keys:
-                values = page_labels.get_label_values(key)
-                nodes = page_labels.get_labeled_nodes(key)
+            for key in page.labels.label_keys:
+                values = page.labels.get_label_values(key)
+                nodes = page.labels.get_labeled_nodes(key)
                 expected = len(values)
                 actual = len(nodes)
                 total += actual
@@ -58,7 +59,7 @@ class Validator:
                     page.valid = False
                     warnings.warn(
                         f'Found {actual} < {expected} nodes labeled ' +
-                        f'{key!r}{get_selector_str(key)}={values!r} ' +
+                        f'{self.get_selector_str(page, key)}={values!r} ' +
                         f'({page.html_path!r}).')
 
                 # Check that labeled nodes are not empty.
@@ -67,25 +68,39 @@ class Validator:
                         page.valid = False
                         xpath = awe.data.html_utils.get_xpath(node)
                         warnings.warn(
-                            f'Node {xpath!r} labeled {key!r}' +
-                            f'{get_selector_str(key)} is empty ' +
+                            f'Node {xpath!r} labeled ' +
+                            f'{self.get_selector_str(page, key)} is empty ' +
                             f'({page.html_path!r}).')
 
             if total == 0:
                 page.valid = False
                 warnings.warn(f'Nothing labeled in page {page.html_path!r}.')
 
-        # Check that extracted visual DOM has the same structure as parsed DOM.
         if self.visuals:
             page_dom = page.dom
             page_dom.init_nodes()
             page_visuals = page.load_visuals()
+
+            # Check that extracted visual DOM has the same structure as parsed
+            # DOM.
             try:
                 page_visuals.fill_tree(page_dom)
             except RuntimeError as e:
                 page.valid = False
                 warnings.warn(
                     f'Cannot fill page visuals ({page.html_path!r}): {e}')
+
+            # Check that all target nodes have a bounding box.
+            if self.labeled_boxes:
+                page_dom.init_labels(propagate_to_leaves=False)
+                for label_key, labeled_nodes in page_dom.labeled_nodes.items():
+                    for n in labeled_nodes:
+                        if n.box is None:
+                            page.valid = False
+                            warnings.warn(
+                                f'Node {n.get_xpath()!r} labeled ' +
+                                f'{self.get_selector_str(page, label_key)} ' +
+                                f'has no bounding box ({page.html_path!r}).')
 
         if page.valid is None:
             page.valid = True
