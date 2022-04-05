ec9c93d5800afbc36a2a03a69426bf7322e0a082
Jan Jones <jjones@outlook.cz>
2022-04-05 16:34:48 +0000

    Wrapp the whole plotting in a function

--- awe/data/visual/exploration.py
+++ awe/data/visual/exploration.py
@@ -1,12 +1,30 @@
 import math
+import os
 import sys
 
 import matplotlib.cm
 import matplotlib.patches
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 
 import awe.data.set.pages
 
+
+def plot_websites(websites: list[awe.data.set.pages.Website]):
+    return plot_pages([
+        next(p for p in w.pages if os.path.exists(p.screenshot_path))
+        for w in websites
+    ])
+
+def plot_pages(pages: list[awe.data.set.pages.Page]):
+    fig, axs = plt.subplots(len(pages),
+        figsize=(10, len(pages) * 20),
+        facecolor='white'
+    )
+    for ax, page in tqdm(zip(axs, pages), desc='pages', total=len(pages)):
+        PageExplorer(page).plot_screenshot_with_boxes(ax)
+        ax.set_title(page.website.name)
+    return fig
 
 class PageExplorer:
     def __init__(self, page: awe.data.set.pages.Page):
