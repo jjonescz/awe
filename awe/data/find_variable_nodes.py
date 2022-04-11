525987e1da82db59b6b542f20f55095112195bd1
Jan Joneš <jjones@outlook.cz>
2022-04-11 15:32:39 +0000

    Initialize page DOM when finding variable nodes

--- awe/data/find_variable_nodes.py
+++ awe/data/find_variable_nodes.py
@@ -16,8 +16,14 @@ def main():
         website: awe.data.set.pages.Website
         p.set_description(website.name)
         website.get_variable_xpaths(
-            dom_selector=lambda p: p.dom
+            dom_selector=get_page_dom
         )
+
+def get_page_dom(page: awe.data.set.pages.Page):
+    page_dom = page.dom
+    page_dom.init_nodes()
+    page_dom.init_labels()
+    return page_dom
 
 if __name__ == '__main__':
     main()
