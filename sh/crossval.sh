2dd3674a27bccac6c3abb0bfbdabb32fa9f4d103
Jan Joneš <jjones@outlook.cz>
2022-04-14 07:46:11 +0000

    Add argument to cross-validation script

--- sh/crossval.sh
+++ sh/crossval.sh
@@ -4,7 +4,10 @@
 
 set -e
 
-for i in $(seq 0 $(python -m awe.training.crossval --print-max-index 2>/dev/null))
+START_INDEX=${1:-0}
+END_INDEX=$(python -m awe.training.crossval --print-max-index 2>/dev/null)
+echo "Running cross-validation from $START_INDEX to $END_INDEX..."
+for i in $(seq $START_INDEX $END_INDEX)
 do
     date
     python -m awe.training.crossval -i $i -c 1
