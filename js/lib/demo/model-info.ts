bdd4b26b22f10e15c17962fa6366c005e528f377
Jan Joneš <jjones@outlook.cz>
2022-04-22 16:44:52 +0200

    Sort versions naturally

--- js/lib/demo/model-info.ts
+++ js/lib/demo/model-info.ts
@@ -1,6 +1,7 @@
 import { readdir, readFile } from 'fs/promises';
 import path from 'path';
 import { LOG_DIR } from '../constants';
+import naturalCompare from 'natural-compare-lite';
 
 export interface ModelInfo {
   labels: string[];
@@ -12,8 +13,13 @@ export interface ModelInfo {
 
 export async function loadModel() {
   const versions = await readdir(LOG_DIR);
+
+  // Sort versions like a human would.
+  versions.sort(naturalCompare);
+
   // Choose only the last version for now.
   const versionName = versions.pop()!;
+
   const infoPath = path.join(LOG_DIR, versionName, 'info.json');
   const infoText = await readFile(infoPath, { encoding: 'utf-8' });
   return JSON.parse(infoText) as ModelInfo;
