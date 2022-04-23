eddf021d850a516b8d7a9706e42bf2b9e2646db5
Jan Jones <jjones@outlook.cz>
2022-04-23 15:36:41 +0000

    Remove unused root notebooks and scripts

--- summarize.py
+++
@@ -1,8 +0,0 @@
-from awe.data import swde
-from awe import utils
-
-sds = swde.Dataset(suffix='-exact')
-pages = [p for v in sds.verticals for w in v.websites for p in w.pages]
-summary = utils.summarize_pages(pages)
-for key, value in summary.items():
-    print(f'{key}: {value}')
