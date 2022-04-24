2b58877a30fe93047ca1015ba5c5fd4e98d3b029
Jan Jones <jjones@outlook.cz>
2022-04-24 08:59:39 +0000

    Add second argument to crossval script

--- sh/crossval.sh
+++ sh/crossval.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Run: `./sh/crossval.sh`.
+# First (optional) argument: start index of cross-validation fold (0-9).
+# Second (optional) argument: end index of cross-validation fold (0-9).
 
 set -e
 
 START_INDEX=${1:-0}
-END_INDEX=$(python -m awe.training.crossval --print-max-index 2>/dev/null)
+END_INDEX=${2:-$(python -m awe.training.crossval --print-max-index 2>/dev/null)}
 echo "Running cross-validation from $START_INDEX to $END_INDEX..."
 for i in $(seq $START_INDEX $END_INDEX)
 do
