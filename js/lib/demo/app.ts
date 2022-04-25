de15c75298e5618f969450575d4c10f0653bb100
Jan Jones <jjones@outlook.cz>
2022-04-25 13:19:01 +0000

    Encapsulate per-page inference

--- js/lib/demo/app.ts
+++ js/lib/demo/app.ts
@@ -1,15 +1,11 @@
 import express, { Request, Response } from 'express';
 import puppeteer from 'puppeteer-core';
 import { Logger } from 'winston';
-import { Extractor } from '../extractor';
 import { logFile } from '../logging';
-import { PageInfo } from '../page-info';
-import { PageRecipe } from '../page-recipe';
-import { ScrapeVersion } from '../scrape-version';
 import { tryParseInt } from '../utils';
 import { loadModel, ModelInfo } from './model-info';
-import { Inference, InferenceOutput, NodePrediction } from './python';
-import * as views from './views';
+import { PageInference } from './page-inference';
+import { Inference } from './python';
 
 export class DemoOptions {
   public readonly debug: boolean;
@@ -43,13 +39,13 @@ export class DemoOptions {
 }
 
 export class DemoApp {
-  private readonly python: Inference | null = null;
-  private browser: Promise<puppeteer.Browser> | puppeteer.Browser;
+  public readonly python: Inference | null = null;
+  public browser: Promise<puppeteer.Browser> | puppeteer.Browser;
 
   private constructor(
-    private readonly options: DemoOptions,
-    private readonly log: Logger,
-    private readonly model: ModelInfo
+    public readonly options: DemoOptions,
+    public readonly log: Logger,
+    public readonly model: ModelInfo
   ) {
     // Create Express HTTP server.
     const app = express();
@@ -107,241 +103,11 @@ export class DemoApp {
   }
 
   private mainPage = async (req: Request, res: Response) => {
-    // Parse parameters.
-    const url = req.query['url']?.toString() ?? '';
-    const timeout = tryParseInt(req.query['timeout'], this.options.timeout);
-    const log = this.log.child({ url: url });
-    log.debug('run');
-
-    // Start writing response so it's asynchronous.
-    res.setHeader('Transfer-Encoding', 'chunked');
-    res.flushHeaders();
-    res.write(views.layoutStart());
-    res.write(views.info(this.model));
-    res.write(views.form(this.model, { url }));
-    if (url === '') {
-      // Return empty form if no URL was provided.
-      res.write(views.layoutEnd());
-      res.end();
-      return;
-    }
-    res.write(views.logStart());
-
-    // Wait for Puppeteer.
-    if (this.browser instanceof Promise) {
-      log.debug('wait for Puppeteer');
-      res.write(views.logEntry('Waiting for Puppeteer one-time init...'));
-      this.flushChunk(res);
-      this.browser = await this.browser;
-    }
-
-    // Run through Puppeteer.
-    log.debug('load page');
-    res.write(views.logEntry('Loading page...'));
-    this.flushChunk(res);
-    let page = await this.browser.newPage();
-    page.setDefaultTimeout(timeout * 1000);
-    const nav1 = await DemoApp.wrapNavigation(
-      (o) => page.goto(url, o),
-      res,
-      log
-    );
-    if (nav1 === 'fail') return;
-
-    // Capture snapshot. This can fail if navigation timeouts at a bad time.
-    log.debug('snapshot');
-    res.write(views.logEntry('Freezing page...'));
-    this.flushChunk(res);
-    let snapshot: string;
+    const inference = new PageInference(this, req, res);
     try {
-      const cdp = await page.target().createCDPSession();
-      const { data } = await cdp.send('Page.captureSnapshot', {
-        format: 'mhtml',
-      });
-      snapshot = data;
-    } catch (e) {
-      const error = e as Error;
-      log.error('snapshot fail', { error: error?.stack });
-      res.write(views.logEntry('Capturing snapshot failed.'));
-      this.flushChunk(res);
+      await inference.run();
+    } finally {
+      await inference.close();
     }
-
-    // Page must be recreated if it timeout occurred.
-    if (nav1 === 'timeout') {
-      log.debug('recreating page');
-      try {
-        page.close();
-      } catch (e) {
-        log.error('page closing failed', { error: (e as Error)?.stack });
-      }
-      page = await this.browser.newPage();
-      page.setDefaultTimeout(timeout * 1000);
-    }
-
-    // Freeze the page.
-    log.debug('freeze page');
-    try {
-      page.setJavaScriptEnabled(false);
-    } catch (e) {
-      const error = e as Error;
-      log.debug('freeze fail', { error: error?.stack });
-      res.write(views.logEntry('Reloading failed.'));
-      this.flushChunk(res);
-    }
-    const nav2 = await DemoApp.wrapNavigation(
-      (o) => page.setContent(snapshot, o),
-      res,
-      log
-    );
-    if (nav2 === 'fail') return;
-
-    // Extract HTML. This can fail if navigation timeouts at a bad time.
-    log.debug('extract content');
-    let html: string;
-    try {
-      html = await page.content();
-    } catch (e) {
-      const error = e as Error;
-      log.error('content fail', { error: error?.stack });
-      res.write(views.logEntry('HTML extraction failed.'));
-      res.end();
-      return;
-    }
-
-    // Extract visuals.
-    log.debug('extract visuals');
-    res.write(views.logEntry('Extracting visuals...'));
-    this.flushChunk(res);
-    const pageInfo = new PageInfo(url, url, html, /* isSwde */ false);
-    const recipe = new PageRecipe(pageInfo, ScrapeVersion.Exact);
-    const extractor = new Extractor(page, recipe, log, /* extractXml */ false);
-    await extractor.extract();
-    const visuals = extractor.data;
-
-    // Take screenshot.
-    log.debug('screenshot');
-    res.write(views.logEntry('Taking screenshot...'));
-    this.flushChunk(res);
-    const screenshot = (await page.screenshot({
-      fullPage: true,
-      encoding: 'base64',
-    })) as string;
-
-    // Log full inputs if desired.
-    if (this.options.logInputs) {
-      log.silly('inputs', { html, visuals, screenshot });
-    }
-
-    // Pass HTML and visuals to Python.
-    let response: InferenceOutput;
-    if (this.python !== null) {
-      // Wait for Python inference to fully load.
-      const pythonLoading = this.python.loading;
-      if (pythonLoading !== null) {
-        log.debug('wait for Python');
-        res.write(views.logEntry('Waiting for inference one-time init...'));
-        this.flushChunk(res);
-        await pythonLoading;
-      }
-
-      // Call Python inference.
-      log.debug('inference');
-      res.write(views.logEntry('Running inference...'));
-      this.flushChunk(res);
-      response = await this.python.send({ url, html, visuals, screenshot });
-    } else {
-      // Mock inference.
-      response = {
-        screenshot,
-        pages: [
-          {
-            engine: [],
-            fuel_economy: [
-              {
-                url: null,
-                confidence: 4.112531661987305,
-                probability: 0.9950769543647766,
-                text: '22 / 29 mpg',
-                xpath:
-                  '/html/body/div[1]/main/div[1]/section[2]/div[1]/div/div[5]/div/div[1]/div[1]/div[2]/div[2]/text()',
-              },
-              {
-                url: null,
-                confidence: 1.1698349714279175,
-                probability: 0.6030704975128174,
-                text: '5/5',
-                xpath:
-                  '/html/body/div[1]/main/div[1]/section[2]/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[2]/text()',
-              },
-            ],
-            model: [],
-            price: [
-              {
-                url: null,
-                confidence: 1.6609370708465576,
-                probability: 0.6562321186065674,
-                text: '$25,377',
-                xpath:
-                  '/html/body/div[1]/main/div[1]/section[2]/div[1]/div/div[3]/div/table/tbody/tr[1]/td[3]/text()',
-              },
-            ],
-          },
-        ],
-      };
-    }
-    res.write(views.logEntry('Rendering response...'));
-    this.flushChunk(res);
-
-    // Log full inputs if they haven't been logged already and there was an
-    // error during inference.
-    let rows = [];
-    if ('error' in response) {
-      if (!this.options.logInputs) log.silly('inputs', { html, visuals });
-    } else {
-      // Render table with results.
-      const pagePred = response.pages[0];
-      for (const [labelKey, nodePreds] of Object.entries<NodePrediction[]>(
-        pagePred
-      )) {
-        for (const nodePred of nodePreds) {
-          rows.push({ labelKey, ...nodePred });
-        }
-      }
-      rows.sort((x, y) => y.confidence - x.confidence);
-    }
-
-    res.write(views.logEntry('Done.'));
-    res.write(views.logEnd());
-    res.write(views.results(rows, response.screenshot));
-    res.write(views.layoutEnd());
-    res.end();
   };
-
-  private static async wrapNavigation(
-    navigator: (options: puppeteer.WaitForOptions) => Promise<unknown>,
-    res: Response,
-    log: Logger
-  ) {
-    try {
-      await navigator({ waitUntil: 'networkidle0' });
-    } catch (e) {
-      const error = e as Error;
-      if (error?.name === 'TimeoutError') {
-        log.debug('goto timeout', { error: error?.stack });
-        res.write(views.logEntry('Navigation timeout.'));
-        return 'timeout';
-      } else {
-        log.debug('goto failed', { error: error?.stack });
-        res.write(views.logEntry(`Navigation failed: ${error}`));
-        res.end();
-        return 'fail';
-      }
-    }
-  }
-
-  private flushChunk(res: Response) {
-    if (this.options.largeChunk !== null) {
-      res.write(this.options.largeChunk);
-    }
-  }
 }
---
+++ js/lib/demo/page-inference.ts
@@ -0,0 +1,276 @@
+import { Request, Response } from 'express';
+import puppeteer from 'puppeteer-core';
+import { Logger } from 'winston';
+import { Extractor } from '../extractor';
+import { PageInfo } from '../page-info';
+import { PageRecipe } from '../page-recipe';
+import { ScrapeVersion } from '../scrape-version';
+import { tryParseInt } from '../utils';
+import { DemoApp } from './app';
+import { InferenceOutput } from './python';
+import * as views from './views';
+
+/** Inference of a page. */
+export class PageInference {
+  private readonly log: Logger;
+  private readonly url: string;
+  private readonly timeout: number;
+  private page: puppeteer.Page = null!;
+
+  public constructor(
+    private readonly app: DemoApp,
+    private readonly req: Request,
+    private readonly res: Response
+  ) {
+    // Parse parameters.
+    this.url = this.req.query['url']?.toString() ?? '';
+    this.timeout = tryParseInt(
+      this.req.query['timeout'],
+      this.app.options.timeout
+    );
+    this.log = this.app.log.child({ url: this.url });
+  }
+
+  public async run() {
+    this.log.debug('run');
+
+    // Start writing response so it's asynchronous.
+    this.res.setHeader('Transfer-Encoding', 'chunked');
+    this.res.flushHeaders();
+    this.res.write(views.layoutStart());
+    this.res.write(views.info(this.app.model));
+    this.res.write(views.form(this.app.model, { url: this.url }));
+    if (this.url === '') {
+      // Return empty form if no URL was provided.
+      this.res.write(views.layoutEnd());
+      this.res.end();
+      return;
+    }
+    this.res.write(views.logStart());
+
+    // Wait for Puppeteer.
+    if (this.app.browser instanceof Promise) {
+      this.log.debug('wait for Puppeteer');
+      this.res.write(views.logEntry('Waiting for Puppeteer one-time init...'));
+      this.flushChunk();
+      this.app.browser = await this.app.browser;
+    }
+
+    // Run through Puppeteer.
+    this.log.debug('load page');
+    this.res.write(views.logEntry('Loading page...'));
+    this.flushChunk();
+    this.page = await this.app.browser.newPage();
+    this.page.setDefaultTimeout(this.timeout * 1000);
+    const nav1 = await this.wrapNavigation((o) => this.page.goto(this.url, o));
+    if (nav1 === 'fail') return;
+
+    // Capture snapshot. This can fail if navigation timeouts at a bad time.
+    this.log.debug('snapshot');
+    this.res.write(views.logEntry('Freezing page...'));
+    this.flushChunk();
+    let snapshot: string;
+    try {
+      const cdp = await this.page.target().createCDPSession();
+      const { data } = await cdp.send('Page.captureSnapshot', {
+        format: 'mhtml',
+      });
+      snapshot = data;
+    } catch (e) {
+      const error = e as Error;
+      this.log.error('snapshot fail', { error: error?.stack });
+      this.res.write(views.logEntry('Capturing snapshot failed.'));
+      this.flushChunk();
+    }
+
+    // Page must be recreated if it timeout occurred.
+    if (nav1 === 'timeout') {
+      this.log.debug('recreating page');
+      await this.close();
+      this.page = await this.app.browser.newPage();
+      this.page.setDefaultTimeout(this.timeout * 1000);
+    }
+
+    // Freeze the page.
+    this.log.debug('freeze page');
+    try {
+      this.page.setJavaScriptEnabled(false);
+    } catch (e) {
+      const error = e as Error;
+      this.log.debug('freeze fail', { error: error?.stack });
+      this.res.write(views.logEntry('Reloading failed.'));
+      this.flushChunk();
+    }
+    const nav2 = await this.wrapNavigation((o) =>
+      this.page.setContent(snapshot, o)
+    );
+    if (nav2 === 'fail') return;
+
+    // Extract HTML. This can fail if navigation timeouts at a bad time.
+    this.log.debug('extract content');
+    let html: string;
+    try {
+      html = await this.page.content();
+    } catch (e) {
+      const error = e as Error;
+      this.log.error('content fail', { error: error?.stack });
+      this.res.write(views.logEntry('HTML extraction failed.'));
+      this.res.end();
+      return;
+    }
+
+    // Extract visuals.
+    this.log.debug('extract visuals');
+    this.res.write(views.logEntry('Extracting visuals...'));
+    this.flushChunk();
+    const pageInfo = new PageInfo(this.url, this.url, html, /* isSwde */ false);
+    const recipe = new PageRecipe(pageInfo, ScrapeVersion.Exact);
+    const extractor = new Extractor(
+      this.page,
+      recipe,
+      this.log,
+      /* extractXml */ false
+    );
+    await extractor.extract();
+    const visuals = extractor.data;
+
+    // Take screenshot.
+    this.log.debug('screenshot');
+    this.res.write(views.logEntry('Taking screenshot...'));
+    this.flushChunk();
+    const screenshot = (await this.page.screenshot({
+      fullPage: true,
+      encoding: 'base64',
+    })) as string;
+
+    // Log full inputs if desired.
+    if (this.app.options.logInputs) {
+      this.log.silly('inputs', { html, visuals, screenshot });
+    }
+
+    // Pass HTML and visuals to Python.
+    let response: InferenceOutput;
+    if (this.app.python !== null) {
+      // Wait for Python inference to fully load.
+      const pythonLoading = this.app.python.loading;
+      if (pythonLoading !== null) {
+        this.log.debug('wait for Python');
+        this.res.write(
+          views.logEntry('Waiting for inference one-time init...')
+        );
+        this.flushChunk();
+        await pythonLoading;
+      }
+
+      // Call Python inference.
+      this.log.debug('inference');
+      this.res.write(views.logEntry('Running inference...'));
+      this.flushChunk();
+      response = await this.app.python.send({
+        url: this.url,
+        html,
+        visuals,
+        screenshot,
+      });
+    } else {
+      // Mock inference.
+      response = {
+        screenshot,
+        pages: [
+          {
+            engine: [],
+            fuel_economy: [
+              {
+                url: null,
+                confidence: 4.112531661987305,
+                probability: 0.9950769543647766,
+                text: '22 / 29 mpg',
+                xpath:
+                  '/html/body/div[1]/main/div[1]/section[2]/div[1]/div/div[5]/div/div[1]/div[1]/div[2]/div[2]/text()',
+              },
+              {
+                url: null,
+                confidence: 1.1698349714279175,
+                probability: 0.6030704975128174,
+                text: '5/5',
+                xpath:
+                  '/html/body/div[1]/main/div[1]/section[2]/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[2]/text()',
+              },
+            ],
+            model: [],
+            price: [
+              {
+                url: null,
+                confidence: 1.6609370708465576,
+                probability: 0.6562321186065674,
+                text: '$25,377',
+                xpath:
+                  '/html/body/div[1]/main/div[1]/section[2]/div[1]/div/div[3]/div/table/tbody/tr[1]/td[3]/text()',
+              },
+            ],
+          },
+        ],
+      };
+    }
+    this.res.write(views.logEntry('Rendering response...'));
+    this.flushChunk();
+
+    // Log full inputs if they haven't been logged already and there was an
+    // error during inference.
+    let rows = [];
+    if ('error' in response) {
+      if (!this.app.options.logInputs)
+        this.log.silly('inputs', { html, visuals });
+    } else {
+      // Render table with results.
+      const pagePred = response.pages[0];
+      for (const [labelKey, nodePreds] of Object.entries(pagePred)) {
+        for (const nodePred of nodePreds) {
+          rows.push({ labelKey, ...nodePred });
+        }
+      }
+      rows.sort((x, y) => y.confidence - x.confidence);
+    }
+
+    this.res.write(views.logEntry('Done.'));
+    this.res.write(views.logEnd());
+    this.res.write(views.results(rows, response.screenshot));
+    this.res.write(views.layoutEnd());
+    this.res.end();
+  }
+
+  public async close() {
+    try {
+      await this.page.close();
+    } catch (e) {
+      const error = e as Error;
+      this.log.error('closing failed', { error: error?.stack });
+    }
+  }
+
+  private async wrapNavigation(
+    navigator: (options: puppeteer.WaitForOptions) => Promise<unknown>
+  ) {
+    try {
+      await navigator({ waitUntil: 'networkidle0' });
+    } catch (e) {
+      const error = e as Error;
+      if (error?.name === 'TimeoutError') {
+        this.log.debug('goto timeout', { error: error?.stack });
+        this.res.write(views.logEntry('Navigation timeout.'));
+        return 'timeout';
+      } else {
+        this.log.debug('goto failed', { error: error?.stack });
+        this.res.write(views.logEntry(`Navigation failed: ${error}`));
+        this.res.end();
+        return 'fail';
+      }
+    }
+  }
+
+  private flushChunk() {
+    if (this.app.options.largeChunk !== null) {
+      this.res.write(this.app.options.largeChunk);
+    }
+  }
+}
